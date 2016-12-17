0cfb711111d19749870ce7b726c32e228e6a2eb0
laplandin <deniskakhv@gmail.com>
2016-12-17 16:21:22 +0300

    page-title positioned, index-nav - styled, items - grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,53 @@
-//window.addEventListener('click', function() {
-    var source = $('#article-item-template').html();
-    var template = Handlebars.compile(source);
-    var context = { articles:[
-        {
-            title: 'Системы видеонаблюдения',
-            description: 'Что-то про системы видеонаблюдения',
-            src: 'img/img1'},
-        {
-            title: 'Охранно-пожарная сигнализация',
-            description: 'Что-то про охранно-пожарную сигнализацию',
-            src: 'img/img1'},
-        {
-            title: 'Системы управления доступом',
-            description: 'Что-то про Системы управления доступом',
-            src: 'img/img1'},
-        {
-            title: 'Автомониторинг',
-            description: 'Что-то про системы видеонаблюдения',
-            src: 'img/img1'},
-        {
-            title: 'Умный дом',
-            description: 'Что-то про системы видеонаблюдения',
-            src: 'img/img1'},
-        {
-            title: 'Серверное и сетевое оборудование',
-            description: 'Что-то про сетевое оборудование',
-            src: 'img/img1'},
-        {
-            title: 'Автоматизация технологических процессов',
-            description: 'Что-то про автоматизацию',
-            src: 'img/img1'}]
-    };
+window.onload = function() {
 
-    var htmlready = template(context);
-    $('.inside').append(htmlready);
+    function renderItems() {
+        var source = $('#article-item-template').html();
+        var template = Handlebars.compile(source);
+        var context = { articles:[
+            {
+                title: 'Системы видеонаблюдения',
+                description: 'Что-то про системы видеонаблюдения',
+                src: 'img/img1'},
+            {
+                title: 'Охранно-пожарная сигнализация',
+                description: 'Что-то про охранно-пожарную сигнализацию',
+                src: 'img/img1'},
+            {
+                title: 'Системы управления доступом',
+                description: 'Что-то про Системы управления доступом',
+                src: 'img/img1'},
+            {
+                title: 'Автомониторинг',
+                description: 'Что-то про системы видеонаблюдения',
+                src: 'img/img1'},
+            {
+                title: 'Умный дом',
+                description: 'Что-то про системы видеонаблюдения',
+                src: 'img/img1'},
+            {
+                title: 'Серверное и сетевое оборудование',
+                description: 'Что-то про сетевое оборудование',
+                src: 'img/img1'},
+            {
+                title: 'Автоматизация технологических процессов',
+                description: 'Что-то про автоматизацию',
+                src: 'img/img1'}]
+        };
 
-//});
+        Handlebars.registerHelper("ifWide", function(index) {
+            index = index +1;
+            if ((index == 2) || ((index - 2) % 5  == 0)) {
+                return "content__card--wide";
+            } else {
+                return "content__card--small";
+            }
+        });
+
+        var htmlready = template(context);
+        $('.inside').append(htmlready);
+    }
+
+
+
+    renderItems();
+};
